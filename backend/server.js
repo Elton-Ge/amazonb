aa86c1e150224298df2623053769191caa974de6
elton <gexin7648@hotmail.com>
2021-08-29 20:46:32 +1000

    "add_mongodb_server"

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
 import express from 'express'
-import data from "./data.js";
+import mongoose from "mongoose";
+import userRouter from "./routers/userRouter.js";
+import productRouter from "./routers/productRouter.js";
 
 const app = express()
-app.get("/api/products/:id", (req, res) => {
-    const product=data.products.find((product) => (
-        product._id === req.params.id
-    ))
-    if (product){
-        res.send(product)
-    }else{
-        res.status(404).send({message: 'Product not found'})
-    }
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/amazonb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
 })
-app.get("/api/products", (req, res) => {
-    res.send(data.products)
-})
+
+app.use('/api/users', userRouter)
+app.use('/api/products', productRouter)
 app.get("/", (req, res) => {
     res.send("server is alive")
+})
+app.use((error, req, res) => {
+    res.status(500).send({message: error.message})
 })
 const port = process.env.PORT || 5000
 app.listen(port, () => {
